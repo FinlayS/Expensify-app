9976828f6c59e0d1b99b101eb19f830698935dc0
FinlayS <fsgm01@gmail.com>
2020-01-14 16:54:07 +0000

    initialise

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />,
+ReactDOM.render(<p> This is my React Boilerplate</p>,
     document.getElementById('app'));
