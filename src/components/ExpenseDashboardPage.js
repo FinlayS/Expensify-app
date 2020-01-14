2962d01a24f4a6e482e6dc1887bd08f5edae9956
FinlayS <fsgm01@gmail.com>
2020-01-14 19:26:17 +0000

    initial commit

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ExpenseDashboardPage = () => (
+    <div>
+        <p>This is my expense dashboard component</p>
+    </div>
+);
+
+export default ExpenseDashboardPage
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <div><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></div>
+        <div><NavLink to="/create" activeClassName="is-active">Create expense</NavLink></div>
+        <div><NavLink to="/edit" activeClassName="is-active">Edit expense</NavLink></div>
+        <div><NavLink to="/help" activeClassName="is-active">Get help</NavLink></div>
+    </header>
+);
+
+export default Header
