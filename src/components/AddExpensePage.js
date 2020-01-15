1cd186f474a28a8c756e1e4f1a47519ee6f7f0ae
FinlayS <fsgm01@gmail.com>
2020-01-15 15:42:09 +0000

    push from new mac

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AddExpensePage = () => (
     <div>
-        <p>This is my add expense component</p>
+        <p>This is my add expense components</p>
     </div>
 
 );
