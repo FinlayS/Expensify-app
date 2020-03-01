d00e69a96c29bc492d143b87a202bf2463023a26
FinlayS <fsgm01@gmail.com>
2020-03-01 13:41:43 +0000

    adding filtering

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import getVisibleExpenses from './selectors/expenses';
@@ -12,13 +13,25 @@ const store = configureStore()
 
 console.log(store.getState())
 
-store.dispatch(addExpense({description: 'Water Bill', amount: 500, createdAt: 1546633}));
+store.dispatch(addExpense({description: 'Water Bill', amount: 544400}));
 store.dispatch(addExpense({description: 'Gas Bill', amount: 6600, createdAt: 1542633}));
-store.dispatch(setTextFilter('BILL'));
+store.dispatch(addExpense({description: 'Rent', amount: 19500}));
+
+store.dispatch(setTextFilter('WATER'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('bill'))
+},3000)
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 
-ReactDOM.render(<AppRouter />,
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx,
     document.getElementById('app'));
