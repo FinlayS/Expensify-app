94ec8640bfbb6f365bc3441943466f75a77dc3bf
FinlayS <fsgm01@gmail.com>
2020-03-04 14:37:58 +0000

    snapshot tests

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,5 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+// setup file
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({
-  adapter: new Adapter()
-});
+configure({ adapter: new Adapter() });
