c1ab9402954db3d4be4d05ab7065f19b352eabd2
FinlayS <fsgm01@gmail.com>
2020-03-29 10:13:30 +0100

    last test done

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {ExpenseListFilters} from "../../components/ExpenseListFilters"
 import {filters, altFilters} from "../fixtures/filters";
+import moment from "moment";
 
 let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
 
@@ -14,7 +15,7 @@ beforeEach(() => {
   wrapper = shallow(
     <ExpenseListFilters
       filters={filters}
-      setTetFilter={setTextFilter}
+      setTextFilter={setTextFilter}
       sortByDate={sortByDate}
       sortByAmount={sortByAmount}
       setStartDate={setStartDate}
@@ -36,21 +37,40 @@ test('should render expenseListFilters with altFilters', () => {
 
 //  (spies)
 test ('should handle text change', () => {
-
+  const value = 'rent';
+  wrapper.find('input').simulate('change', {
+    target: { value }
+  });
+   expect(setTextFilter).toHaveBeenLastCalledWith(value)
 });
 //  (spies)
 test ('should sort by date', () => {
-
+  wrapper.setProps({
+    filters: altFilters
+  });
+  const value = 'date';
+  wrapper.find('select').simulate('change', {
+    target: { value }
+  });
+  expect(sortByDate).toHaveBeenCalled()
 });
 //  (spies)
 test ('should sort by amount', () => {
-
+  const value = 'amount';
+  wrapper.find('select').simulate('change', {
+    target: { value }
+  });
+  expect(sortByAmount).toHaveBeenCalled()
 });
 //  (spies)
 test ('should handle date changes', () => {
-
+  const startDate = new moment(0).subtract(14, 'days').valueOf();
+  wrapper.find('select').prop(setStartDate(startDate))
+  expect(setStartDate).toHaveBeenCalled()
 });
 //  (assert state, simulate change on e.target)
 test ('should handle date focus changes', () => {
-
+  const endDate = new moment(0).add(14, 'days').valueOf();
+  wrapper.find('select').prop(setEndDate(endDate))
+  expect(setEndDate).toHaveBeenCalled()
 });
